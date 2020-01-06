4324c387bb4f3ee8cd4212c7364c1eb7bb917341
yi-shiyu <shiyu.weilan@gmail.com>
2020-01-06 11:53:54 +0800

    Update trojan_nginx.sh

--- trojan_nginx.sh
+++ trojan_nginx.sh
@@ -123,11 +123,7 @@ sleep 1
 curl https://get.acme.sh | sh
 
 ~/.acme.sh/acme.sh --issue -d $url --webroot /usr/share/nginx/html/
-systemctl stop nginx.service
-~/.acme.sh/acme.sh --installcert -d $url \
---key-file $INSTALLPREFIX/$NAME/private.key \
---fullchain-file $INSTALLPREFIX/$NAME/fullchain.cer \
---reloadcmd "systemctl force-reload nginx.service"
+~/.acme.sh/acme.sh --installcert -d $url --key-file $INSTALLPREFIX/$NAME/private.key --fullchain-file $INSTALLPREFIX/$NAME/fullchain.cer
 
 echo '安装trojan配置文件'
 cat > $CONFIGPATH << EOF
