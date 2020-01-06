902783730adfd89d28cefecfd543a1b7694bc151
yi-shiyu <shiyu.weilan@gmail.com>
2020-01-06 20:25:55 +0800

    Update install_rsa.sh

--- install_rsa.sh
+++ install_rsa.sh
@@ -28,19 +28,12 @@ while true; do
     fi
 done
 
-
-DISABLE_PW_LOGIN=0
-
-if [[ "${pass}" == "y" || "${pass}" == "Y" ]]; then
-    DISABLE_PW_LOGIN=1
-fi
-
 #check if we are root
-if [ $EUID -ne 0 ]; then
+if [[ $EUID != 0 ]]; then
     echo 'Error: you need to be root to run this script'; exit 1;
 fi
 
-if [ ! -f "${HOME}/.ssh/authorized_keys" ]; then
+if [[ ! -f "${HOME}/.ssh/authorized_keys" ]]; then
     echo "Info: ~/.ssh/authorized_keys is missing ...";
 
     echo "Creating ${HOME}/.ssh/authorized_keys ..."
@@ -57,14 +50,17 @@ fi
 #get key from server
 curl -D /tmp/headers.txt -L ${url} >/tmp/key.txt >> /dev/null 2>&1 &
 HTTP_CODE=$(sed -n 's/HTTP\/1\.[0-9] \([0-9]\+\).*/\1/p' /tmp/headers.txt | tail -n 1)
-if [[ $HTTP_CODE -ne 200 ]]; then
+if [[ $HTTP_CODE != 200 ]]; then
     echo "Error: download server error"; exit 1;
 fi
 
 PUB_KEY=$(cat /tmp/key.txt)
 
+if [[ ${PUB_KEY:0:7} != "ssh-rsa" ]]; then
+    echo "Warning: Key format error"; exit 1;
+fi
 
-if [ $(grep -m 1 -c "${PUB_KEY}" ${HOME}/.ssh/authorized_keys) -eq 1 ]; then
+if [[ $(grep -m 1 -c "${PUB_KEY}" ${HOME}/.ssh/authorized_keys) -eq 1 ]]; then
     echo 'Warning: Key is already installed'; exit 1;
 fi
 
@@ -75,7 +71,7 @@ rm -f /tmp/headers.txt
 echo 'Key installed successfully'
 
 #disable root password
-if [ ${DISABLE_PW_LOGIN} -eq 1 ]; then
+if [[ "${pass}" == "y" || "${pass}" == "Y" ]]; then
     sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
     echo 'Disabled password login in SSH'
     echo 'Restart SSHd!'
